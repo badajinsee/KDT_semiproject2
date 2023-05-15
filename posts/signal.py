7ae3f4b7f046b28ae8d4ae598a5dc43c3b33c04b
komanduk <102152679+komanduk@users.noreply.github.com>
2023-05-15 18:27:54 +0900

    edit연동

--- posts/signal.py
+++ posts/signal.py
@@ -1,21 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Post, Comment, Notification
+from .models import Comment
+from django.urls import reverse
+from notifications.signals import notify
 
-@receiver(post_save, sender=Post.like_users)
-def create_like_notification(sender, instance, created, **kwargs):
-    if created:
-        post = instance.post
-        sender = instance.user
-        receiver = post.user
-        if sender != receiver:
-            Notification.objects.create(post=post, sender=sender, receiver=receiver, notification_type=Notification.LIKE)
-            
 @receiver(post_save, sender=Comment)
-def create_comment_notification(sender, instance, created, **kwargs):
-    if created:
-        post = instance.post
-        sender = instance.user
-        receiver = post.user
-        if sender != receiver:
-            Notification.objects.create(post=post, sender=sender, receiver=receiver, notification_type=Notification.COMMENT)
+def send_comment_notification(sender, instance, **kwargs):
+    post = instance.post
+    comment_user = instance.user
+    post_user = post.user
+    if comment_user != post_user:
+            # 알림 생성
+            recipient = post_user
+            verb = f"{comment_user.username}님이 답글을 남겼습니다."
+            action_object = instance
+            url = reverse('detail', kwargs={'pk': post.pk})
+            notify.send(comment_user, recipient=recipient, verb=verb, action_object=action_object, url=url)
